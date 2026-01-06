5e18d0de112f5e83c56547e1bbbdf2e54ff6887c
Omer-Faruk-Ruzgar <ruzgaromerfaruk@outlook.com>
2026-01-06 05:14:08 +0100

    main(): added code for running and comparing baseline and WTGA optimization

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+from optimizer.ga_runner import run_ga
+
+def main():
+    np.random.seed(42)
+
+    print("\n--- Baseline ---")
+    run_ga(use_wtga = False, plot = True)
+
+    print("\n--- WTGA ---")
+    run_ga(use_wtga = True, plot = True)
+
+if __name__ == "__main__":
+    main()
